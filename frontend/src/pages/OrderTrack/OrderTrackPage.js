4a49f80f9df680fa724a66d53486b04eb0e90249
Avijit Sharma <avijitsharma.6290@gmail.com>
2024-10-21 17:42:17 +0530

    food mine commit-1

---
+++ frontend/src/pages/OrderTrack/OrderTrackPage.js
@@ -0,0 +1,77 @@
+import React, { useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { trackOrderById } from '../../services/orderService';
+import NotFound from '../../components/NotFound/NotFound';
+import classes from './orderTrackPage.module.css';
+import DateTime from '../../components/DateTime/DateTime';
+import OrderItemsList from '../../components/OrderItemsList/OrderItemsList';
+import Title from '../../components/Title/Title';
+import Map from '../../components/Map/Map';
+
+export default function OrderTrackPage() {
+  const { orderId } = useParams();
+  const [order, setOrder] = useState();
+
+  useEffect(() => {
+    orderId &&
+      trackOrderById(orderId).then(order => {
+        setOrder(order);
+      });
+  }, [orderId]);
+
+  if (!orderId)
+    return <NotFound message="Order Not Found" linkText="Go To Home Page" />;
+
+  return (
+    order && (
+      <div className={classes.container}>
+       
+        <div className={classes.content}>
+        <Title title="Track Your Order" margin="1.5rem 0 0 .2rem" fontSize="1.9rem" />
+          <h1>Order #{order.id}</h1>
+          <div className={classes.header}>
+            <div>
+              <strong>Date</strong>
+              <DateTime date={order.createdAt} />
+            </div>
+            <div>
+              <strong>Name</strong>
+              {order.name}
+            </div>
+            <div>
+              <strong>Address</strong>
+              {order.address}
+            </div>
+            <div>
+              <strong>State</strong>
+              {order.status}
+            </div>
+            {order.paymentId && (
+              <div>
+                <strong>Payment ID</strong>
+                {order.paymentId}
+              </div>
+            )}
+          </div>
+
+          <OrderItemsList order={order} />
+        </div>
+
+        <div>
+          <Title title="Your Location" fontSize="1.6rem" />
+          <Map location={order.addressLatLng} readonly={true} />
+        </div>
+
+        <div className={classes.payment}>
+        <Link to={`/`}>Go to Home Page</Link>
+        </div>
+
+        {order.status === 'NEW' && (
+          <div className={classes.payment}>
+            <Link to="/payment">Go To Payment</Link>
+          </div>
+        )}
+      </div>
+    )
+  );
+}
